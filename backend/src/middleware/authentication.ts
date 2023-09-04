36c6c34d2397cc76a2373c4d02269156859ef3ee
Jonas List <81174108+Shadowcrafter88@users.noreply.github.com>
2023-09-04 15:26:38 +0200

    Update authentication.ts

--- backend/src/middleware/authentication.ts
+++ backend/src/middleware/authentication.ts
@@ -9,7 +9,7 @@ export const authenticateUser = () => {
 
     if (!token) {
       console.log("No token found");
-      
+
       return res.status(401).json({ error: "Unauthorized: Missing token" });
     }
 
@@ -26,7 +26,7 @@ export const authenticateUser = () => {
 };
 
 // Accepts a response object and a user object. Creates a JWT with the user's ID, email, and username, sets a cookie with the token, and returns the token. The cookie expires in 1 hour.
-export const setTokenCookie = (res: Response, user: User) => {
+export const setTokenCookie = (req: Request, res: Response, user: User) => {
   const secret = process.env.JWT_SECRET as string;
   const payload = {
     id: user.id,
@@ -36,12 +36,21 @@ export const setTokenCookie = (res: Response, user: User) => {
   };
   const token = jwt.sign(payload, secret, { expiresIn: "5d" });
 
-  res.cookie("jwt", token, {
-    maxAge: 1000 * 60 * 60 * 24 * 5, // 5 days
-    domain: 'localhost',
-    secure: false, // set to true if your using https
-  });
+  const maxAge = 1000 * 60 * 60 * 24 * 5; // 5 days
+
+  if (req.headers.host === "127.0.0.1") {
+    res.cookie("jwt", token, {
+      maxAge,
+      domain: "127.0.0.1",
+      secure: false, // set to true if your using https
+    });
+  } else if (req.headers.host?.includes("danl.ddns.net")) {
+    res.cookie("jwt", token, {
+      maxAge,
+      domain: "danl.ddns.net",
+      secure: false, // set to true if your using https
+    });
+  }
 
   return token;
 };
-
